104240a74401b42229c62d6e0fb36bd61338c108
Denis Cordero <deniscordero1987@gmail.com>
2021-04-06 21:54:03 -0600

    adding chessboard.js

--- exercises/chapter_2_program_structure/FizzBuzz.js
+++ exercises/chapter_2_program_structure/FizzBuzz.js
@@ -15,3 +15,10 @@ for (let i = 1; i <= 100; i++) {
     console.log((i % 3 == 0 && i % 5 == 0) ? 'FizzBuzz' : ((i % 3 == 0) ? 'Buzz' : ((i % 5 == 0) ? 'Fizz' : i)));
 
 }
+
+for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+    console.log(output || n);
+}
--- exercises/chapter_2_program_structure/looping_a_triangle.js
+++ exercises/chapter_2_program_structure/looping_a_triangle.js
@@ -5,3 +5,7 @@ for (let i = 0; i < patron.length; i++) {
     triangle += patron[i];
     console.log(triangle);
 }
+
+
+for (let line = "#"; line.length < 8; line += "#")
+    console.log(line);
